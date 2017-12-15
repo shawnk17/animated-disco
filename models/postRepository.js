06d9ececcf652c1090e229d114f9ef37bf142f97
shawn <axe2781@gmail.com>
2017-12-15 11:20:31 -0500

    uhou

--- models/postRepository.js
+++ models/postRepository.js
@@ -6,6 +6,30 @@ const fs = require('fs'),
 let postList = [{ permalink: "post-1", title: "Post 1", postContent: "<h1>This is my first post</h1>", author: "shawn"},
 { permalink: "post-2", title: "Post 2", postContent: "<h1>This is my second post</h1>", author: "shawn"}];
 
+let loadPosts = () => {
+    fs.readFile(fileName, "utf8", (err, data) => {
+        if (err) {
+            console.error("Error loading data file: " + err.message);
+            throw err;
+        } else {
+            let newPostsArr = JSON.parse(data);
+            if (newPostsArr.length > 0) {
+                postList = newPostsArr;
+            }
+        }
+    });
+};
+
+let savePosts = () => {
+    fs.writeFile(fileName, JSON.stringify(postList), (err) => {
+        if (err) {
+            console.error("Error writing the file." + err.message);
+            throw err;
+        }
+        console.log("The file has been saved.");
+    });
+};
+
 let repo = {
     dataSource : "Filesystem",
     postCount: () => {
--- routes/newPost.js
+++ routes/newPost.js
@@ -3,20 +3,22 @@ let router = express.Router();
 let repo = require('../models/postRepository');
 
 // GET sends the user the new post page with the form to enter the post
-router.get("/", (req,res,next) => {
+router.get("/", (req, res, next) => {
     res.render('newpost', { title: 'New Post' });
 });
 
 // POST receives the data from the user about the new post
-router.post("/", (req,res,next) => {
-    let newPost = {};
+router.post("/", (req, res, next) => {
+    if (req.body.passphrase === "passphrase") {
+        let newPost = {};
 
-    newPost.title = req.body.title;
-    newPost.permalink = req.body.permalink;
-    newPost.postContent = req.body.postContent;
-    newPost.author = req.body.author;
+        newPost.title = req.body.title;
+        newPost.permalink = req.body.permalink;
+        newPost.postContent = req.body.postContent;
+        newPost.author = req.body.author;
 
-    repo.addPost(newPost);
+        repo.addPost(newPost);
+    }
 
     res.redirect("/");
 });
