7d5a993477ce03e754d6eac3a85019d612e45991
shawn <axe2781@gmail.com>
2017-12-26 15:12:04 -0500

    fix text

--- routes/blog.js
+++ routes/blog.js
@@ -2,16 +2,14 @@ var express = require('express');
 var router = express.Router();
 let repo = require('../models/postRepository');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('blog', {
-      title: 'Shawn Bloog Site',
-      author: "Shawn",
-      dataSource: repo.dataSource,
-      postCount: repo.postCount(),
-      posts: repo.getPosts()
-    });
+  res.render('blog', {
+    title: 'Shawn Bloog Site',
+    author: "Shawn",
+    dataSource: repo.dataSource,
+    postCount: repo.postCount(),
+    posts: repo.getPosts()
   });
-  
+});
 
 module.exports = router;
--- routes/newPost.js
+++ routes/newPost.js
@@ -2,12 +2,10 @@ let express = require('express');
 let router = express.Router();
 let repo = require('../models/postRepository');
 
-// GET sends the user the new post page with the form to enter the post
 router.get("/", (req, res, next) => {
     res.render('newpost', { title: 'New Post' });
 });
 
-// POST receives the data from the user about the new post
 router.post("/", (req, res, next) => {
     if (req.body.passphrase === "passphrase") {
         let newPost = {};
